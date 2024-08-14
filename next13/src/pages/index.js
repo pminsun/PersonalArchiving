223df1bfaedb68e3b45fbb3b7893f1896e6fd6cb
minsun309 <pminsun309@gmail.com>
2024-08-14 17:28:46 +0900

    Feat: 풀스트린 스크롤 추가

--- next13/src/pages/index.js
+++ next13/src/pages/index.js
@@ -1,7 +1,12 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <main className="home_container">
       <h1>개인 아카이빙</h1>
+      <div className="link_area">
+        <Link href={'/fullpagescroll'}>fullpagescroll</Link>
+      </div>
     </main>
   );
 }
