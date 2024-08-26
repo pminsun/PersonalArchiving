8ecc99a644753bdc501487f290464b6cd428990d
minsun309 <pminsun309@gmail.com>
2024-08-26 10:44:56 +0900

    Design: 헤더 스타일 변경 / scroll페이지css 분리

--- next13/src/pages/_app.js
+++ next13/src/pages/_app.js
@@ -1,6 +1,7 @@
 import Layout from '@/components/Layout';
 import '@/styles/globals.css';
 import '@/styles/codeHighlightColors.css';
+import '@/styles/scrollStyle.css';
 
 export default function App({ Component, pageProps }) {
   return (
