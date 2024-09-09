99f681361d689ed8fe12d24a1bb0ce45f46ed3bb
minsun309 <pminsun309@gmail.com>
2024-09-09 14:20:28 +0900

    Feat : 뒤로가기 버튼 추가

--- next13/src/components/Header.js
+++ next13/src/components/Header.js
@@ -1,9 +1,22 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { FaArrowLeft } from 'react-icons/fa';
 
 export default function Header() {
+  const router = useRouter();
   return (
     <header>
-      <Link href={'/'}>
+      <button
+        type="button"
+        onClick={() => router.back()}
+        className="back"
+      >
+        <FaArrowLeft />
+      </button>
+      <Link
+        href={'/'}
+        className="home"
+      >
         <h1>개인 아카이빙</h1>
       </Link>
     </header>
